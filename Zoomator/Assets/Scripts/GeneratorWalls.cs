659bfcee32847ea4f28916be9957a89c17646501
PyTechMike <pytechmike@gmail.com>
2018-09-22 19:53:12 +0300

    GeneratorWalls changet to beta 16-bands version

--- Zoomator/Assets/Scripts/GeneratorWalls.cs
+++ Zoomator/Assets/Scripts/GeneratorWalls.cs
@@ -7,29 +7,29 @@ public class GeneratorWalls : MonoBehaviour {
 	public GameObject wall;
 	public GameObject coin;
 
-	private float[] FirstAudioBand = new float[2];
-	private float[] SecondAudioBand = new float[2];
+	private float[] nowAudioBands = new float[16];
+	private float[] lastAudioBands = new float[16];
+	private float plusBandDifference; 
+
 	public float delay;
 	private bool canSpawn;
 
 	public static float lastXPos = 1f;
 	public static float lastYPos = 6f;
-
 	public static int count;
 
 	public static bool spawnWithLastPos = false;
-
 	public static bool isFirstWall = true;
 
 	IEnumerator WallGener() {
 		yield return new WaitForSeconds(0.1f);
-		
-		FirstAudioBand[1] = FirstAudioBand[0];
-		FirstAudioBand[0] = FirstAudioListener.FirstAudioBandBuffer; 
-		
-		SecondAudioBand[1] = SecondAudioBand[0];
-		SecondAudioBand[0] = FirstAudioListener.SecondAudioBandBuffer; 
-		
+
+		for (int i = 0; i < 16; i++) {
+			lastAudioBands[i] = nowAudioBands[i]; 
+
+			nowAudioBands[i] = FirstAudioListener._audioBandBuffer[i];
+		}
+
 		if(canSpawn) {  
 			Instantiate (wall);	
 			yield return new WaitForSeconds(0.05f);
@@ -42,37 +42,20 @@ public class GeneratorWalls : MonoBehaviour {
 	}
 
 	void Start() {
-		FirstAudioBand[0] = 0;
-		FirstAudioBand[1] = 0;
-
-		SecondAudioBand[0] = 0;
-		SecondAudioBand[1] = 0;
-
 		canSpawn = false;
 		
 		StartCoroutine(WallGener());
 	}
 	void Update() {
-		if(FirstAudioListener.middleAudioBandBuffer > 0.7f) {
-			if(FirstAudioBand[0] > FirstAudioBand[1] + 0.1f || SecondAudioBand[0] > SecondAudioBand[1] + 0.1f) {  
-				canSpawn = true;	
-			} else {
-				canSpawn = false;
+		for (int i = 0; i < 16; i++) {
+			if(FirstAudioListener.middleAudioBandBuffer > 0.7f) {
+				plusBandDifference = 0.07f;
+			} else if(FirstAudioListener.middleAudioBandBuffer > 0.4f) {
+				plusBandDifference = 0.05f;
+			} else if(FirstAudioListener.middleAudioBandBuffer > 0.2f) {
+				plusBandDifference = 0.03f;
 			}
-		} else if(FirstAudioListener.middleAudioBandBuffer > 0.3f) {
-			if(FirstAudioBand[0] > FirstAudioBand[1] + 0.12f || SecondAudioBand[0] > SecondAudioBand[1] + 0.12f) {  
-				canSpawn = true;	
-			} else {
-				canSpawn = false;
-			}
-		} else if(FirstAudioListener.middleAudioBandBuffer > 0.2f) {
-			if(FirstAudioBand[0] > FirstAudioBand[1] + 0.08f || SecondAudioBand[0] > SecondAudioBand[1] + 0.08f) {  
-				canSpawn = true;	
-			} else {
-				canSpawn = false;
-			}
-		} else {
-			if(FirstAudioBand[0] > FirstAudioBand[1] + 0.03f || SecondAudioBand[0] > SecondAudioBand[1] + 0.03f) {  
+			if(lastAudioBands[i] > nowAudioBands[i] + plusBandDifference) {  
 				canSpawn = true;	
 			} else {
 				canSpawn = false;
